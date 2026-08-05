2ec1dfe71c5dbbb95bd7610e371fc375a48f60aa
Sean Murphy <sean@iamseanmurphy.com>
2026-08-05 10:45:16 +0000

    6.6.4

--- webp-converter-for-media/vendor/composer/installed.php
+++ webp-converter-for-media/vendor/composer/installed.php
@@ -3,7 +3,7 @@
         'name' => 'gbiorczyk/webp-converter-for-media',
         'pretty_version' => 'dev-master',
         'version' => 'dev-master',
-        'reference' => '26e08bbae81350f36074a4b5b0d8dd4a25127001',
+        'reference' => '90bdcecd6f7311914ae31554b711d59f72bdd30c',
         'type' => 'library',
         'install_path' => __DIR__ . '/../../',
         'aliases' => array(),
@@ -13,7 +13,7 @@
         'gbiorczyk/webp-converter-for-media' => array(
             'pretty_version' => 'dev-master',
             'version' => 'dev-master',
-            'reference' => '26e08bbae81350f36074a4b5b0d8dd4a25127001',
+            'reference' => '90bdcecd6f7311914ae31554b711d59f72bdd30c',
             'type' => 'library',
             'install_path' => __DIR__ . '/../../',
             'aliases' => array(),
